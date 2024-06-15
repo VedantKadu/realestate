4b452d69cf74e9eaede2ca3c164ab4a5b346168d
Vedant <kvedant339@gmail.com>
2024-06-15 23:18:12 +0530

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUP from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
-export default function App() {
-  return <div className="text-red-600">Vedant</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUP />} />
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
