f4156f48819c06c7877343e1339c87aadc7fd5e6
Vedant <kvedant339@gmail.com>
2024-06-16 11:37:26 +0530

    database connection setup done

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
-import express from "express"; //ES6 syntax
 // const express = require("express"), this is CommonJS syntax
+import express from "express"; //ES6 syntax
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
