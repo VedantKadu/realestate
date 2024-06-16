178f05dc5b6fa38bb93c156deceed52c2e8abc8a
Vedant <kvedant339@gmail.com>
2024-06-16 11:56:03 +0530

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model.apply("User", userSchema);
+
+export default User;
