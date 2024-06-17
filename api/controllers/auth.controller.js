230ca6190db4e953dc1252ebede91bba58031395
Vedant <kvedant339@gmail.com>
2024-06-17 22:35:57 +0530

    Signup functionality created with showing error if any occurs and navigated to sign-in on successful signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,12 +4,13 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  console.log(req.body);
   const hashedPass = bcryptjs.hashSync(password, 10);
   try {
     const newUser = new User({ username, email, password: hashedPass });
     await newUser.save();
     res.status(201).json("user crated succesfully");
   } catch (err) {
-    next(errorHandler(550, "Custom error"));
+    next(errorHandler(550, err.message));
   }
 };
